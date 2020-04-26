63a0f3d1b97a54799ea8eac6e6412eca0ccb7855
mhs121 <62454499+mhs121@users.noreply.github.com>
2020-04-26 16:52:12 +0800

    main

---
+++ src/array/repeat.java
@@ -0,0 +1,19 @@
+package array;
+
+public class repeat {
+    public static void main(String[] args) {
+        int isiGelas = 0;
+        int penuh = 225;
+
+        System.out.println("Isi Gelas sekarang : "+isiGelas"ml");
+        System.out.println("isi Glesa Ketika Penuh :"+isiGelas"ml");
+
+        while (isiGelas != penuh){
+            isiGelas++;
+            System.out.println("Sedang mengisi gelas..");
+            System.out.println("isi gelas Sekarang :"+isiGelas"ml");
+        }
+
+        System.out.println("Finale: Isi  Gelas sekarang :"+isiGelas"ml");
+    }
+}
