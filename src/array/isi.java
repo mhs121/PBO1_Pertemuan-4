908079df840950683f3404fd7fbb9c470c6b24bb
mhs121 <62454499+mhs121@users.noreply.github.com>
2020-04-27 09:28:24 +0800

    main

---
+++ src/array/isi.java
@@ -0,0 +1,17 @@
+package array;
+
+public class isi {
+    public static void main(String[] args) {
+        int isi_enetron = 0;
+        int full = 99;
+
+        System.out.println("enetron mengisi :"+isi_enetron+"% Power");
+        System.out.println("Enetron terisi :"+isi_enetron+"% power");
+
+        for (isi_enetron=0; isi_enetron <= full; isi_enetron++){
+         System.out.println("Mengisi Enetron Power");
+         System.out.println("Enetron tersedia :"+isi_enetron+"% Power");
+        }
+        System.out.println("Full Charge Energy Enetron Tersedia :"+isi_enetron+"% Power Maksimum");
+    }
+}
