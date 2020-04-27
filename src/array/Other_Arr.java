46caa68a379cfb8e61aa24199f2d32b4cbf523a3
mhs121 <62454499+mhs121@users.noreply.github.com>
2020-04-27 10:13:27 +0800

    main

---
+++ src/array/Other_Arr.java
@@ -0,0 +1,13 @@
+package array;
+
+public class Other_Arr {
+    public static void main(String[] args) {
+        String[][] Rider = {{"Decade","Change Form & Power other Rider"}, {"Zio","Summon & Power other rider"}, {"Zero one","Infinity Power"}};
+
+        for (int i = 0; i <Rider.length; i++) {
+            System.out.println("Kamen Rider Type :" + Rider[i][0]);
+            System.out.println("Special Power    :" + Rider[i][1]);
+            System.out.println("--------------------------");
+        }
+    }
+}
